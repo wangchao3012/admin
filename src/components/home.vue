fbe787045c8a178a1b8f04dc6ee4837706641583
wangchao3012 <wangchao3012@163.com>
2017-02-24 17:22:29 +0800

    构建后台管理模板

---
+++ src/components/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="home">
+   首页{{msg}}
+    </div> 
+</template>
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ src/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        首页模板{{ msg }}
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'index',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                menu: [
+                    {
+                        name: '首页',
+                        icon: '',
+                    },
+                    {
+                        name: '用户管理',
+                        icon: '',
+                    }
+                ]
+            }
+        }
+    }
+
+</script>
