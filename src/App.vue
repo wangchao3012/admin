6b53a7c7bf4a45960a14c9971067e8a35d00814b
wangchao3012 <wangchao3012@163.com>
2017-03-09 17:36:05 +0800

    构建项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <div id="div1"></div>
+    		<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">点我弹出/隐藏弹出框</button>
+	
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted(){
+    $('#div1').text('123456');
+
+				$('[data-toggle="popover"]').popover() 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Index from './index'
+import router from './router'
+
+Vue.config.productionTip = false
+
+import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css'
+
+import '!style-loader!css-loader!font-awesome/css/font-awesome.css'
+import '!style-loader!css-loader!@/assets/AdminLTE/dist/css/skins/_all-skins.css'
+import '!style-loader!css-loader!@/assets/AdminLTE/dist/css/AdminLTE.css'
+
+import $ from '@/assets/jquery-vendor.js'
+import 'bootstrap'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<Index/>',
+  components: { Index }
+})
